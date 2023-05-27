8d66fc7aebbdaf1de03f2120eec46e797586fdf5
l4mers <gustav@henrikssons.org>
2023-05-28 00:18:31 +0200

    config

---
+++ src/main/java/com/restapi/restapi/config/CorsConfiguration.java
@@ -0,0 +1,21 @@
+package com.restapi.restapi.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class CorsConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://127.0.0.1:5174") // Replace with your client's origin
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("Authorization", "Content-Type")
+                .exposedHeaders("Authorization")
+                .allowCredentials(true);
+    }
+}
